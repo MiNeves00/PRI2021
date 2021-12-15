cf12e1f36427e21829e89c976262031fcabfea5b
João Gonçalves <up201806162@fe.up.pt>
2021-12-15 19:32:42 +0000

    items added to solr

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ COPY data/csv/new_champions.csv /data/champions.csv
 
 COPY data/schema.json /data/schema.json
 
+COPY data/csv/new_items.csv /data/items.csv
+
+COPY data/items_schema.json /data/items_schema.json
+
 COPY startup.sh /scripts/startup.sh
 
 ENTRYPOINT ["/scripts/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 precreate-core champions
-
+precreate-core items
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
@@ -10,11 +10,15 @@ sleep 10
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
-    http://localhost:8983/solr/champions/schema
+    http://localhost:8983/solr/champions/schema 
 
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/items_schema.json \
+    http://localhost:8983/solr/items/schema 
 
 # Populate collection
 bin/post -c champions /data/champions.csv
+bin/post -c items /data/items.csv
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
